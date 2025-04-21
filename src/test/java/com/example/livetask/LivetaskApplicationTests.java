bcf35fdf62c037ae51e982b08303bb849e7d342f
hamao_kazane <hamao_kazane@plise.co.jp>
2025-04-21 11:28:55 +0900

    test commit

---
+++ src/test/java/com/example/livetask/LivetaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.livetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivetaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
