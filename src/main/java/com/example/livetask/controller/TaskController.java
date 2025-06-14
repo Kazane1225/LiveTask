eddae180ec6399ad7d52e4cbea518b679a281869
kazane1225 <kajya.h1225@gmail.com>
2025-06-15 00:39:18 +0900

    リファクタリング

--- src/main/java/com/example/livetask/controller/TaskController.java
+++ src/main/java/com/example/livetask/controller/TaskController.java
@@ -2,24 +2,19 @@ package com.example.livetask.controller;
 
 import com.example.livetask.model.Task;
 import com.example.livetask.model.User;
-import com.example.livetask.repository.TaskRepository;
 import com.example.livetask.repository.UserRepository;
-import com.example.livetask.service.ChatGPTService;
 import com.example.livetask.service.TaskService;
 
 import jakarta.validation.Valid;
 
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,20 +25,15 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class TaskController {
     
-    private final TaskRepository taskRepository;
     private final UserRepository userRepository;
 
     @Autowired
     private MessageSource messageSource;
 
     @Autowired
-    private ChatGPTService chatGPTService;
-
-    @Autowired
     private TaskService taskService;
 
-    public TaskController(TaskRepository taskRepository, UserRepository userRepository) {
-        this.taskRepository = taskRepository;
+    public TaskController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
--- src/main/java/com/example/livetask/service/TaskService.java
+++ src/main/java/com/example/livetask/service/TaskService.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
